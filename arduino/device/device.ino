69e0338dc822bd54d0e4a79f4e5f493c6a9291a6
aidanbyrnes <136363128+aidanbyrnes@users.noreply.github.com>
2026-04-02 23:18:20 -0500

    Performance enhancements and extended features

--- arduino/device/device.ino
+++ arduino/device/device.ino
@@ -1,25 +1,23 @@
 #include <WiFi.h>
 #include <esp_now.h>
 #include <CodeCell.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <freertos/queue.h>
 
-#define DEVICE_ID 10
+#define DEVICE_ID 1
 #define SAMPLERATE 20 // Hz
-#define SEND_RATE 10  // Hz
 
 #define RMS_WINDOW 200 // milliseconds
 #define DEADZONE .1
+#define WAKEUP_THRESHOLD 1.0f
 #define LP_CUTOFF_FREQ 2.0f
 #define MAX_FROZEN 5
+#define RADIO_OFF_RESTART_MS (10UL * 60UL * 1000UL) // 30min in milliseconds
+#define STILL_TIMEOUT_MS 30000 // milliseconds
 
 CodeCell myCodeCell;
 
-//AC:A7:04:D4:DA:88
-uint8_t receiverMAC[] = {0xAC, 0xA7, 0x04, 0xD4, 0xDA, 0x88}; // Change to receiver MAC address
+uint8_t receiverMAC[] = {0xAC, 0xA7, 0x04, 0xD4, 0xDA, 0x88};
 
-#define SENSOR_DATA_LEN 5 // 3 slots reserved for future use
+#define SENSOR_DATA_LEN 8
 
 typedef struct __attribute__((packed)) {
   unsigned int deviceID;
@@ -27,22 +25,31 @@ typedef struct __attribute__((packed)) {
   float data[SENSOR_DATA_LEN];
 } DataPacket;
 
-TaskHandle_t sensorTaskHandle = NULL;
-TaskHandle_t espnowTaskHandle = NULL;
-QueueHandle_t sensorDataQueue;
+#define RMS_WINDOW_SAMPS ((int)((float)RMS_WINDOW / 1000.0f * SAMPLERATE))
 
-float rms_accum = 0.0;
+float rms_buffer[RMS_WINDOW_SAMPS] = {0.0f};;
+int rms_head = 0;
+float rms_sum_sq = 0.0f;
+
 float rms = 0.0;
 float lp = 0.0;
 float lp_alpha = 0.0;
-int rms_index = 0;
-int rms_samps;
 int frozen_frames = 0;
+
+bool send_data = true;
+bool radio_on = true;
+unsigned long still_since = 0;
+unsigned long radioOffSince = 0; // tracks when radio was turned off
+
+bool is_idle = false;
+bool is_charging = false;
 
 float ax = 0, ay = 0, az = 0;
 float qw = 0, qx = 0, qy = 0, qz = 0;
 float ax_hist = 0, ay_hist = 0, az_hist = 0;
 float qw_hist = 1, qx_hist = 0, qy_hist = 0, qz_hist = 0;
+
+DataPacket packet;
 
 float calc_lp_alpha(float cutoff_freq, float sample_rate) {
   float rc = 1.0f / (2.0f * PI * cutoff_freq);
@@ -50,36 +57,58 @@ float calc_lp_alpha(float cutoff_freq, float sample_rate) {
   return dt / (rc + dt);
 }
 
-int calc_buffer_size(int sr) {
-  int size = (float)RMS_WINDOW / 1000.0f * sr;
-  return (size > 0) ? size : 1;
+void radio_start() {
+  WiFi.mode(WIFI_STA);
+  esp_now_init(); 
+
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(peerInfo.peer_addr, receiverMAC, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  esp_now_add_peer(&peerInfo);
+  radio_on = true;
+  radioOffSince = 0; // clear the off-timer when radio comes back on
 }
 
-void calc_motion_scalar(int sr = SAMPLERATE) {
+void radio_stop() {
+  esp_now_deinit();
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+  radio_on = false;
+  if (radioOffSince == 0) radioOffSince = millis(); // start the off-timer
+}
+
+
+// thorough NaN avoidance
+void calc_motion_scalar() {
   ax_hist = ax;
   ay_hist = ay;
   az_hist = az;
 
   myCodeCell.Motion_LinearAccRead(ax, ay, az);
 
-  rms_accum += ax * ax + ay * ay + az * az;
-  rms_index++;
-  rms_samps = calc_buffer_size(sr);
+  float new_val = ax * ax + ay * ay + az * az;
 
-  if (rms_index >= rms_samps) {
-    rms = sqrt(rms_accum / rms_samps);
-    rms *= (rms >= DEADZONE);
-    rms_index = 0;
-    rms_accum = 0.0;
-  }
+  if (!isfinite(new_val)) new_val = 0.0f;
 
-  lp = rms * lp_alpha + (1.0f - lp_alpha) * lp;
+  rms_sum_sq -= rms_buffer[rms_head];
+  rms_buffer[rms_head] = new_val;
+  rms_sum_sq += new_val;
+  rms_head = (rms_head + 1) % RMS_WINDOW_SAMPS;
+
+  if (rms_sum_sq < 0.0f) rms_sum_sq = 0.0f;
+
+  rms = sqrt(rms_sum_sq / RMS_WINDOW_SAMPS);
+  rms *= (rms >= DEADZONE);
+  
+  float new_lp = rms * lp_alpha + (1.0f - lp_alpha) * lp;
+  if (isfinite(new_lp)) lp = new_lp;
 }
 
 bool freeze_check() {
   if (ax == ax_hist && ay == ay_hist && az == az_hist &&
-      qw == qw_hist && qx == qx_hist &&
-      qy == qy_hist && qz == qz_hist) {
+      qw == qw_hist && qx == qx_hist && qy == qy_hist && qz == qz_hist) {
 
     if (frozen_frames > MAX_FROZEN) {
       Serial.println(">> Freeze detected");
@@ -95,73 +124,27 @@ bool freeze_check() {
   return false;
 }
 
-void sensorTask(void *parameter) {
-
-  const unsigned int send_interval = SAMPLERATE / SEND_RATE;
-  unsigned int sample_count = 0;
-
-  DataPacket packet;
-  packet.deviceID = DEVICE_ID;
-
-  while (1) {
-
-    if (myCodeCell.Run(SAMPLERATE)) {
-
-      calc_motion_scalar();
-
-      qw_hist = qw;
-      qx_hist = qx;
-      qy_hist = qy;
-      qz_hist = qz;
-      myCodeCell.Motion_RotationVectorRead(qw, qx, qy, qz);
-
-      freeze_check();
-      //if (freeze_check()) continue;
-
-      sample_count++;
-
-      if (sample_count >= send_interval) {
-
-        packet.data[0] = lp;
-
-        packet.data[1] = qw;
-        packet.data[2] = qx;
-        packet.data[3] = qy;
-        packet.data[4] = qz;
-
-        packet.battery = (unsigned int)myCodeCell.BatteryLevelRead();
-
-        xQueueSend(sensorDataQueue, &packet, 0);
-
-        sample_count = 0;
-      }
-    }
+void charging_check() {
+  if (myCodeCell.PowerStateRead() == 5) {
+    myCodeCell.LED_SetBrightness(2);
+    myCodeCell.LED_Breathing(LED_COLOR_GREEN);
+    is_charging = true;
+  } else {
+    is_charging = false;
   }
 }
 
-void espnowTask(void *parameter) {
-
-  DataPacket packet;
-
-  while (1) {
-
-    if (xQueueReceive(sensorDataQueue, &packet, portMAX_DELAY) == pdTRUE) {
-
-      esp_err_t result = esp_now_send(
-        receiverMAC,
-        (uint8_t*)&packet,
-        sizeof(packet)
-      );
-
-      if (result != ESP_OK) {
-        Serial.printf(">> Send failed: %d\n", result);
-      }
-    }
+void idle_check() {
+  if (rms < DEADZONE) {
+    if (still_since == 0) still_since = millis();
+    if (millis() - still_since >= STILL_TIMEOUT_MS) is_idle=true;
+  } else {
+    still_since = 0;
+    is_idle = false;
   }
 }
 
 void setup() {
-
   Serial.begin(115200);
 
   myCodeCell.Init(MOTION_LINEAR_ACC + MOTION_ROTATION);
@@ -169,26 +152,59 @@ void setup() {
 
   lp_alpha = calc_lp_alpha(LP_CUTOFF_FREQ, SAMPLERATE);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.setTxPower(WIFI_POWER_8_5dBm);
+  radio_start();
 
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("ESP-NOW init failed");
-    return;
-  }
-  esp_now_peer_info_t peerInfo = {};
-  memcpy(peerInfo.peer_addr, receiverMAC, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false;
-
-  esp_now_add_peer(&peerInfo);
-
-  sensorDataQueue = xQueueCreate(2, sizeof(DataPacket));
-
-  xTaskCreate(sensorTask, "SensorTask", 4096, NULL, 2, &sensorTaskHandle);
-  xTaskCreate(espnowTask, "ESPNowTask", 4096, NULL, 1, &espnowTaskHandle);
+  packet.deviceID = DEVICE_ID;
 }
 
 void loop() {
-  vTaskDelay(portMAX_DELAY);
+  if (myCodeCell.Run(SAMPLERATE)) {
+    myCodeCell.LED_SetBrightness(0);
+
+    // restart if radio has been off for too long (still or charging)
+    if (!radio_on && radioOffSince > 0 && millis() - radioOffSince >= RADIO_OFF_RESTART_MS) {
+      ESP.restart();
+    }
+
+    charging_check();
+
+    calc_motion_scalar();
+    idle_check();
+
+    qw_hist = qw;
+    qx_hist = qx;
+    qy_hist = qy;
+    qz_hist = qz;
+    myCodeCell.Motion_RotationVectorRead(qw, qx, qy, qz);
+
+    freeze_check();
+
+    if(is_charging || is_idle){
+      if(radio_on){
+        radio_stop();
+      }
+      return;
+    }
+    else if(!is_charging && !is_idle && !radio_on){
+      radio_start();
+    }
+
+    if (send_data && radio_on) {
+      packet.data[0] = lp;
+      packet.data[1] = qw;
+      packet.data[2] = qx;
+      packet.data[3] = qy;
+      packet.data[4] = qz;
+
+      packet.battery = (unsigned int)min((int)myCodeCell.BatteryLevelRead(), 100);
+
+      esp_err_t result = esp_now_send(
+        receiverMAC,
+        (uint8_t*)&packet,
+        sizeof(packet)
+      );
+    }
+
+    send_data = !send_data;
+  }
 }
--- arduino/spatial/spatial.ino
+++ arduino/spatial/spatial.ino
@@ -1,18 +1,27 @@
+// Uses code by Jaryd from Core Electronics
+// https://core-electronics.com.au/guides/sensors/diy-2d-and-3d-spatial-tracking-with-ultra-wideband-arduino-and-pico-guide/#XC4IICX
+
 #include <WiFi.h>
 #include <esp_now.h>
 #include <CodeCell.h>
 
-#define DEVICE_ID 18
+#define DEVICE_ID 9
 
 #define RX_PIN 21
 #define TX_PIN 22
 
-#define SEND_RATE 10 // Send every 100ms
+#define SAMPLERATE 20 // Hz
 
-//AC:A7:04:D4:DA:88
+#define RMS_WINDOW 1000 // milliseconds (substantially higher for torso sensor due to gait)
+#define DEADZONE .1
+#define LP_CUTOFF_FREQ 2.0f
+#define MAX_FROZEN 5
+#define RADIO_OFF_RESTART_MS (10UL * 60UL * 1000UL)
+#define STILL_TIMEOUT_MS 30000
+
 uint8_t receiverMAC[] = {0xAC, 0xA7, 0x04, 0xD4, 0xDA, 0x88};
 
-#define SENSOR_DATA_LEN 5
+#define SENSOR_DATA_LEN 8
 #define ID_OFFSET UINT_MAX / 2
 
 typedef struct __attribute__((packed)) {
@@ -21,25 +30,42 @@ typedef struct __attribute__((packed)) {
   float data[SENSOR_DATA_LEN];
 } DataPacket;
 
-DataPacket packet;
+DataPacket uwbPacket;
+DataPacket motionPacket;
 
 CodeCell myCodeCell;
 
-void setup() {
-  Serial.begin(115200);
+bool radio_on = true;
+unsigned long radioOffSince = 0;
 
-  myCodeCell.Init(0);
-  myCodeCell.LED_SetBrightness(0);
+bool send_data = true;
+bool is_idle = false;
+bool is_charging = false;
 
-  Serial1.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);
+#define RMS_WINDOW_SAMPS ((int)((float)RMS_WINDOW / 1000.0f * SAMPLERATE))
 
+float rms_buffer[RMS_WINDOW_SAMPS] = {0.0f};
+int rms_head = 0;
+float rms_sum_sq = 0.0f;
+float rms = 0.0;
+float lp = 0.0;
+float lp_alpha = 0.0;
+
+unsigned long still_since = 0;
+
+float ax = 0, ay = 0, az = 0;
+float ax_hist = 0, ay_hist = 0, az_hist = 0;
+int frozen_frames = 0;
+
+float calc_lp_alpha(float cutoff_freq, float sample_rate) {
+  float rc = 1.0f / (2.0f * PI * cutoff_freq);
+  float dt = 1.0f / sample_rate;
+  return dt / (rc + dt);
+}
+
+void radio_start() {
   WiFi.mode(WIFI_STA);
-  WiFi.setTxPower(WIFI_POWER_8_5dBm);
-
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("ESP-NOW init failed");
-    return;
-  }
+  esp_now_init();
 
   esp_now_peer_info_t peerInfo = {};
   memcpy(peerInfo.peer_addr, receiverMAC, 6);
@@ -47,15 +73,79 @@ void setup() {
   peerInfo.encrypt = false;
 
   esp_now_add_peer(&peerInfo);
+  radio_on = true;
+  radioOffSince = 0;
+}
 
-  packet.deviceID = DEVICE_ID + ID_OFFSET;
-  //packet.battery = 0;
+void radio_stop() {
+  esp_now_deinit();
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+  radio_on = false;
+  if (radioOffSince == 0) radioOffSince = millis();
+}
+
+void calc_motion_scalar() {
+  ax_hist = ax;
+  ay_hist = ay;
+  az_hist = az;
+
+  myCodeCell.Motion_LinearAccRead(ax, ay, az);
+
+  float new_val = ax * ax + ay * ay + az * az;
+  if (!isfinite(new_val)) new_val = 0.0f;
+
+  rms_sum_sq -= rms_buffer[rms_head];
+  rms_buffer[rms_head] = new_val;
+  rms_sum_sq += new_val;
+  rms_head = (rms_head + 1) % RMS_WINDOW_SAMPS;
+
+  if (rms_sum_sq < 0.0f) rms_sum_sq = 0.0f;
+
+  rms = sqrt(rms_sum_sq / RMS_WINDOW_SAMPS);
+  rms *= (rms >= DEADZONE);
+
+  float new_lp = rms * lp_alpha + (1.0f - lp_alpha) * lp;
+  if (isfinite(new_lp)) lp = new_lp;
+}
+
+bool freeze_check() {
+  if (ax == ax_hist && ay == ay_hist && az == az_hist) {
+    if (frozen_frames > MAX_FROZEN) {
+      Serial.println(">> Freeze detected");
+      ESP.restart();
+    } else {
+      frozen_frames++;
+      return true;
+    }
+  } else {
+    frozen_frames = 0;
+  }
+  return false;
+}
+
+void charging_check() {
+  if (myCodeCell.PowerStateRead() == 5) {
+    myCodeCell.LED_SetBrightness(2);
+    myCodeCell.LED_Breathing(LED_COLOR_GREEN);
+    is_charging = true;
+  } else {
+    is_charging = false;
+  }
+}
+
+void idle_check() {
+  if (rms < DEADZONE) {
+    if (still_since == 0) still_since = millis();
+    if (millis() - still_since >= STILL_TIMEOUT_MS) is_idle = true;
+  } else {
+    still_since = 0;
+    is_idle = false;
+  }
 }
 
 bool decodeUwbDistances(uint8_t* data, int dataLen, float* distances) {
-  for (int i = 0; i < SENSOR_DATA_LEN; i++) {
-    distances[i] = -1.0;
-  }
+  for (int i = 0; i < SENSOR_DATA_LEN; i++) distances[i] = -1.0;
 
   if (dataLen < 35) return false;
   if (data[0] != 0xaa || data[1] != 0x25 || data[2] != 0x01) return false;
@@ -67,81 +157,95 @@ bool decodeUwbDistances(uint8_t* data, int dataLen, float* distances) {
                             (data[byteOffset + 1] << 8) |
                             (data[byteOffset + 2] << 16) |
                             (data[byteOffset + 3] << 24);
-
       distances[i] = (distanceRaw > 0) ? distanceRaw / 1000.0 : -1.0;
     }
   }
-
   return true;
 }
 
-void printDistances(float* distances, bool validData) {
-  if (!validData) {
-    Serial.println("Invalid data received");
-    return;
-  }
+void setup() {
+  Serial.begin(115200);
 
-  Serial.println("Base Station Distances:");
-  for (int i = 0; i < SENSOR_DATA_LEN; i++) {
-    Serial.print("  BS");
-    Serial.print(i);
-    if (distances[i] > 0) {
-      Serial.print(": ");
-      Serial.print(distances[i], 3);
-      Serial.println("m");
-    } else {
-      Serial.println(": Not visible");
-    }
-  }
-  Serial.println("------------------------------");
+  myCodeCell.Init(MOTION_LINEAR_ACC);
+  myCodeCell.LED_SetBrightness(0);
+
+  Serial1.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);
+
+  lp_alpha = calc_lp_alpha(LP_CUTOFF_FREQ, SAMPLERATE);
+
+  radio_start();
+
+  uwbPacket.deviceID = DEVICE_ID + ID_OFFSET;
+  motionPacket.deviceID = DEVICE_ID;
 }
 
 void loop() {
-  static uint8_t buffer[256];
-  static int bufferIndex = 0;
-  static bool messageStarted = false;
-  static float latestDistances[SENSOR_DATA_LEN] = {-1.0};
-  static bool hasValidData = false;
-  static unsigned long lastSendTime = 0;
+  if (myCodeCell.Run(SAMPLERATE)) {
+    myCodeCell.LED_SetBrightness(0);
 
-  // Read and decode incoming UWB serial data
-  while (Serial1.available()) {
-    uint8_t incomingByte = Serial1.read();
+    // Restart if radio has been off too long
+    if (!radio_on && radioOffSince > 0 && millis() - radioOffSince >= RADIO_OFF_RESTART_MS) {
+      ESP.restart();
+    }
 
-    if (!messageStarted && incomingByte == 0xAA) {
-      messageStarted = true;
-      bufferIndex = 0;
-      buffer[bufferIndex++] = incomingByte;
-    } else if (messageStarted) {
-      buffer[bufferIndex++] = incomingByte;
+    charging_check();
+    calc_motion_scalar();
+    idle_check();
+    freeze_check();
 
-      if (bufferIndex >= 35) {
-        hasValidData = decodeUwbDistances(buffer, bufferIndex, latestDistances);
-        printDistances(latestDistances, hasValidData);
+    if (is_charging || is_idle) {
+      if (radio_on) radio_stop();
+      return;
+    } else if (!is_charging && !is_idle && !radio_on) {
+      radio_start();
+    }
 
-        messageStarted = false;
+    // Read and decode incoming UWB serial data
+    static uint8_t buffer[256];
+    static int bufferIndex = 0;
+    static bool messageStarted = false;
+    static float latestDistances[SENSOR_DATA_LEN] = {-1.0};
+    static bool hasValidData = false;
+
+    while (Serial1.available()) {
+      uint8_t incomingByte = Serial1.read();
+
+      if (!messageStarted && incomingByte == 0xAA) {
+        messageStarted = true;
         bufferIndex = 0;
+        buffer[bufferIndex++] = incomingByte;
+      } else if (messageStarted) {
+        buffer[bufferIndex++] = incomingByte;
+
+        if (bufferIndex >= 35) {
+          hasValidData = decodeUwbDistances(buffer, bufferIndex, latestDistances);
+          messageStarted = false;
+          bufferIndex = 0;
+        }
+
+        if (bufferIndex >= 256) {
+          messageStarted = false;
+          bufferIndex = 0;
+        }
+      }
+    }
+
+    if (send_data && radio_on) {
+      unsigned int batt = (unsigned int)min((int)myCodeCell.BatteryLevelRead(), 100);
+
+      // Send UWB distances packet
+      if (hasValidData) {
+        uwbPacket.battery = batt;
+        memcpy(uwbPacket.data, latestDistances, sizeof(latestDistances));
+        esp_now_send(receiverMAC, (uint8_t*)&uwbPacket, sizeof(uwbPacket));
       }
 
-      if (bufferIndex >= 256) {
-        messageStarted = false;
-        bufferIndex = 0;
-      }
-    }
-  }
-
-  // Send latest distances at SEND_RATE
-  unsigned long now = millis();
-  if (now - lastSendTime >= 1000.0f / SEND_RATE) {
-    lastSendTime = now;
-
-    if(myCodeCell.Run(SEND_RATE)){
-      packet.battery = (unsigned int)myCodeCell.BatteryLevelRead();
+      // Send motion scalar packet (no ID offset)
+      motionPacket.battery = batt;
+      motionPacket.data[0] = lp;
+      esp_now_send(receiverMAC, (uint8_t*)&motionPacket, sizeof(motionPacket));
     }
 
-    if (hasValidData) {
-      memcpy(packet.data, latestDistances, sizeof(latestDistances));
-      esp_now_send(receiverMAC, (uint8_t*)&packet, sizeof(packet));
-    }
+    send_data = !send_data;
   }
 }
